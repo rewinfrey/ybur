bfec03cd85412715141699b641927c1af760ca13
Rick Winfrey <rewinfrey@github.com>
2019-10-21 14:51:50 -0700

    Create example.rb

---
+++ example.rb
@@ -0,0 +1,24 @@
+class Exaple
+
+  def initialize(name)
+    @name = name
+  end
+  
+  def get_name
+    @name
+  end
+  
+  def set_name(name)
+    @name = name
+  end
+  
+  def do_thing
+    name = get_name
+    puts "hello, #{name}"
+  end
+  
+  def do_thing_with_name(name)
+    set_name(name)
+    do_thing
+  end
+end
